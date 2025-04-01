241c6862cdd03249ec929401622ca7d24b61b728
Вячеслав <slavvva05@gmail.com>
2025-04-01 08:48:57 +0300

    Add versionController and service

---
+++ Api/Controllers/VersionsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.DataContexts;
+using DataLayer.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionsController(AppDbContext context) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<GameVersion>> PostGameVersion(GameVersion gameVersion)
+        {
+            context.GameVersions.Add(gameVersion);
+            await context.SaveChangesAsync();
+
+            return Created("", gameVersion);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGameVersion(int id)
+        {
+            var gameVersion = await context.GameVersions.FindAsync(id);
+            if (gameVersion == null)
+            {
+                return NotFound();
+            }
+
+            context.GameVersions.Remove(gameVersion);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ DataLayer/Services/VersionsService.cs
@@ -0,0 +1,43 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Services
+{
+    public class VersionsService
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "http://localhost:5199/api/Versions/";
+
+        public VersionsService(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new(_baseUrl);
+            _client.GetStringAsync("1");
+        }
+
+        public VersionsService()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new(_baseUrl);
+        }
+
+        public async Task AddVersionAsync(GameVersion version)
+        {
+            HttpResponseMessage response =
+                await _client.PostAsJsonAsync("", version);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteVersionAsync(int id)
+        {
+            HttpResponseMessage response =
+                await _client.DeleteAsync($"{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
